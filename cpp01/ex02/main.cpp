e2c6680a6ca29d34cb13a15a17cd108c29396d02
elpatrinum <meb-bak@outlook.fr>
2024-09-24 16:09:15 +0100

    autosave

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: muel-bak <muel-bak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 20:13:21 by muel-bak          #+#    #+#             */
-/*   Updated: 2024/06/10 20:29:17 by muel-bak         ###   ########.fr       */
+/*   Updated: 2024/09/24 16:06:55 by muel-bak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 int main ()
 {
 	std::string line = "HI THIS IS BRAIN";
-	std::string *ptr = &line;
-	std::string &ref = line;
+	std::string *stringPTR = &line;
+	std::string &stringREF = line;
 	std::cout << "The memory address of the string variable : " << &line << std::endl;
-	std::cout << "The memory address held by stringPTR      : " << ptr << std::endl;
-	std::cout << "The memory address held by stringREF      : " << &ref << std::endl;
+	std::cout << "The memory address held by stringPTR      : " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF      : " << &stringREF << std::endl;
 	std::cout << "The value of the string variable  : " << line << std::endl;
-	std::cout << "The value pointed to by stringPTR : " << *ptr << std::endl;
-	std::cout << "The value pointed to by stringREF : " << ref << std::endl;
+	std::cout << "The value pointed to by stringPTR : " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF : " << stringREF << std::endl;
 	return (0);
 }
