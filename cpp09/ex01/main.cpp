9c6fe724853d7779c23b8887fb817b35cee29744
elpatrinum <meb-bak@outlook.fr>
2024-10-21 17:21:56 +0100

    autosave

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -5,33 +5,9 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: muel-bak <muel-bak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/20 13:08:10 by muel-bak          #+#    #+#             */
-/*   Updated: 2024/10/21 17:21:49 by muel-bak         ###   ########.fr       */
+/*   Created: 2024/10/21 17:15:37 by muel-bak          #+#    #+#             */
+/*   Updated: 2024/10/21 17:15:44 by muel-bak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "BitcoinExchange.hpp"
-
-void f()
-{
-	system("leaks btc");
-}
-
-int main(int ac, char **av)
-{
-	// atexit(f);
-    if (ac !=  2 || !av[1])
-        return (std::cerr << "Usage : ./btc [file.cvs] " << std::endl, 1);
-	std::string file(av[1]);
-    try
-    {
-        BitcoinExchange btc; 
-		btc.check_find(file);
-    }
-    catch (const std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-		return (0);
-	}
-	return(0);
-}
+#include "RPN.hpp"
